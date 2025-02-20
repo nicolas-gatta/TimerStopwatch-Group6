5e5fe116ebf32d2ca975cba89ac42033a6dcfd74
LiocansG <nico6041@hotmail.be>
2025-02-20 18:38:00 +0100

    Adding missing test for LapTime and RunningStopWatch

--- src/test/java/states/StopwatchTests.java
+++ src/test/java/states/StopwatchTests.java
@@ -1,7 +1,9 @@
 package states;
 
 import states.stopwatch.AbstractStopwatch;
+import states.stopwatch.LaptimeStopwatch;
 import states.stopwatch.ResetStopwatch;
+import states.stopwatch.RunningStopwatch;
 import states.timer.AbstractTimer;
 
 import org.junit.jupiter.api.*;
@@ -50,6 +52,63 @@ class StopwatchTests {
 		 * because we used history states		
 		 */
 		assertEquals(current, newState.left());
+	}
+
+	@Test
+	void testRightTransition() {
+		// Start in RunningStopwatch state
+		current = RunningStopwatch.Instance();
+
+		// Simulate pressing "right" button
+		newState = current.right();
+
+		// Verify that the transition is to ResetStopwatch
+		assertSame(ResetStopwatch.Instance(), newState, "Pressing 'right' should transition to ResetStopwatch.");
+	}
+
+	@Test
+	void testUpTransition() {
+		// Start in LaptimeStopwatch state
+		current = LaptimeStopwatch.Instance();
+
+		// Simulate pressing "up" button
+		newState = current.up();
+
+		// Verify that the transition is to RunningStopwatch
+		assertSame(RunningStopwatch.Instance(), newState, "Pressing 'up' should transition to RunningStopwatch.");
+	}
+
+	@Test
+	void testGetUpText() {
+		// Instance of LaptimeStopwatch
+		current = LaptimeStopwatch.Instance();
+
+		// Verify the text returned by getUpText()
+		assertEquals("unsplit", current.getUpText(), "getUpText() should return 'unsplit'.");
+	}
+
+	@Test
+	void testGetDisplayString() {
+		// Instance of LaptimeStopwatch
+		current = LaptimeStopwatch.Instance();
+
+		// Update laptime and timeout = 5;
+		current.entry();
+
+		// Decrease timeout to 0 to get RunningStopwatch;
+		current.doIt();
+		current.doIt();
+		current.doIt();
+		current.doIt();
+
+		newState = current.doIt();
+
+		// Simulate the entry action that sets lapTime
+		current.entry();
+
+		assertEquals(RunningStopwatch.Instance(), newState, "Calling doIt() * 5 should return RunningStopwatch.");
+		// Verify the output format
+		assertEquals("lapTime = 5", current.getDisplayString(), "getDisplayString() should return 'lapTime = X' where X is the lap time.");
 	}
 
 }
